eca303d7d795cc0dc42e906fca0983ebddd612aa
Krystian Rybinski <k.rybinski.581@studms.ug.edu.pl>
2022-11-19 16:43:12 +0100

    zad16

--- zad16.c
+++ zad16.c
@@ -2,28 +2,32 @@
 
 int main()
 {
-    int m; //liczba wpisana przez użytkownika
-    int liczby[] = {1,2,3,4,5}; //Tabela możliwych odpowiedzi
-    int i; //zmienna do petli
-    int j;
-    int index = -1;
-    int size = 4;
-    printf("Odgadnij liczbe z zakresu od 1 do 5: ");
-    scanf("%d",&m);
-    for(i=3;i>=1;i--)
+    int m,i,j,s,k;
+    int liczby[] = {1,2,3,4,5};
+    printf("Odgadnij liczbe z zakresu od 1 do 5\n");
+    for(i=4;i>=1;--i) //ilość szans
     {
-       for(j=0;j<size;j++)
-       {
-        if(liczby[j]==m)
+        printf("Pozostalo %d prob: ",i);
+        scanf("%d",&m);
+        for(j=0;j<=4;j++) //szukamy w naszej tablicy liczby równej wpisanej liczbie
         {
-            for(int k=j;k=size;k++)
+            if(liczby[j]==m)
             {
-                liczby[k]=liczby[k+1];
-                printf("%d ",liczby[k]);
+                liczby[j]=0; // i zamieniamy ją na zero
             }
         }
-       }
-       scanf("%d",&m);
+        printf("To nie ta liczba\n");
     }
+    for(k=0;k<=4;++k)
+        {
+            if(liczby[k]!=0) // wyszukujemy z tablicy liczbę różną od zera, czyli jedyną liczbę, jakiej nie wpisaliśmy w naszych próbach
+            {
+                    s = liczby[k]; // liczbę zapisujemy w zmiennej s
+                    break;
+            }
+        }
+    printf("Nie udalo sie ogdadnac, szukana liczba to %d",s); // program wypluwa "właściwą odpowiedź", i gra gitara
     return 0;
 }
+
+//Trzymaj się tam byku, pamiętaj PIJ WODE
